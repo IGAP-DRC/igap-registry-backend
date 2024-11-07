86b945dc3f752ef7991aa0142f892e8d1567fc0a
riddyndoma <riddyndoma@gmail.com>
2024-11-07 10:06:17 +0100

    Initial commit

---
+++ src/main/java/com/igap/registry/IgapRegistryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.igap.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgapRegistryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgapRegistryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igap/registry/IgapRegistryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igap.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgapRegistryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
