680d0e6d2f3b9f820451aec68c60ceeb38763199
Joe Monkila <jnkiwa25@gmail.com>
2024-12-03 14:49:54 +0100

    Login DTO

---
+++ src/main/java/com/igap/registry/dto/auth/LoginDTO.java
@@ -0,0 +1,16 @@
+package com.igap.registry.dto.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Joe Monkila
+ */
+
+
+@Getter
+@Setter
+public class LoginDTO {
+    private String email;
+    private String password;
+}
